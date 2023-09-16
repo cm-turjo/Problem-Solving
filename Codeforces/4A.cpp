434f19170b29e6065b44a77641e3d29900898081
unknown <chinmoymodak5500@gmail.com>
2023-09-16 15:58:23 +0600

    Codeforces 4A Solved

---
+++ Codeforces/4A.cpp
@@ -0,0 +1,37 @@
+//Chariot_48
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define fast ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0)
+#define endl         '\n'
+#define ter          return 0
+#define PI           acos(-1)
+#define mm7          100000007
+#define mems(a,b)    memset(a,b,sizeof(a))
+#define FL(i,a,b)    for(i=a;i<b;i++)
+#define ll           long long int
+#define v(df)        vector<df>
+#define mp(df1,df2)  map<df1,df2>
+#define pa(df1,df2)  pair<df1,df2>
+#define pb           push_back
+#define it           ::iterator
+//#define INT_MAX    2147483647
+//#define INT_MIN    -2147483648
+
+int main()
+{
+    //Just Basic Implementation Problem
+	fast;
+	int w;
+	cin >> w;
+	if (w == 2 || w == 0)cout << "NO" << endl;
+
+	else if (w % 2 == 0)
+	{
+		cout << "YES" << endl;
+
+	}
+	else cout << "NO" << endl;
+	ter;
+}
