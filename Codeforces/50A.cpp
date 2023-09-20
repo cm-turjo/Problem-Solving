bcfa8fb52ba423567964b0edf34162ef3fa309fc
unknown <chinmoymodak5500@gmail.com>
2023-09-21 02:49:32 +0600

    Codeforces 50A Solved

---
+++ Codeforces/50A.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int m,n,d;
+    cin>>m>>n;
+    d=(m*n)/2;//area wise calculate korsi
+    //area diya divide korlei answer pawa jai
+    //2D grid e area wise calculate !
+    cout<<d<<endl;
+
+}
