a444e8391198ad8713eaad2bd7a8220c14dff98c
unknown <chinmoymodak5500@gmail.com>
2023-09-29 12:28:53 +0600

    Codeforces 486A Solved

---
+++ Codeforces/486A Calculating Function.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+#ifndef ONLINE_JUDGE
+//for getting input from input.txt
+	freopen("input.txt", "r", stdin);
+//for writing output to output.txt
+	freopen("output.txt", "w", stdout);
+#endif
+
+	long long int n;
+	cin >> n;
+	if (n % 2 == 0)cout << n / 2 << endl;
+	else cout << -(n + 1) / 2 << endl;
+	return 0;
+}
