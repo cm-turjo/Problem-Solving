d9208ccc13bd6712f8e97160f51568ef61293b78
unknown <chinmoymodak5500@gmail.com>
2023-10-05 14:47:51 +0600

    Codeforces 520A Solved

---
+++ Codeforces/520A Pangram.cpp
@@ -0,0 +1,43 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+#ifndef ONLINE_JUDGE
+//for getting input from input.txt
+	freopen("input.txt", "r", stdin);
+//for writing output to output.txt
+	freopen("output.txt", "w", stdout);
+#endif
+	int n;
+	cin >> n;
+	string s1;
+	cin >> s1;
+	map<char, int>mp;
+	map<char, int>::iterator it;
+	//first we count the occurence of every letter
+	for (int i = 0; i < s1.size(); i++)
+	{
+		s1[i] = tolower(s1[i]);
+		mp[s1[i]]++;
+	}
+	if (n < 26)//as the problem suggests that the string should contain
+		//every character so if total char count is less than 26 cout no
+	{
+		cout << "NO" << endl;
+	}
+	else
+	{
+		if (mp.size() == 26)//if the map size is equal to 26 that means all
+			//characters are counted one or more time
+		{
+			cout << "YES" << endl;
+		}
+		else
+		{
+			cout << "NO" << endl;
+
+		}
+	}
+	return 0;
+}
